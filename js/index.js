6b4081afed61ae5a94cf9532593366e0b8748794
MYO THIRI <sandarlamin.llm@gmail.com>
2020-10-15 15:09:43 +0900

    Initial commit.Complete the front end portion of project.

---
+++ js/index.js
@@ -0,0 +1,18 @@
+var map;
+
+function initMap() {
+    let losAngeles = {lat: 34.063380, lng: -118.358080}
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: losAngeles,
+        zoom: 8,
+    });
+    
+    createMarker();
+}
+
+const createMarker = () => {
+    var marker = new google.maps.Marker({
+        position: {lat: 34.063380, lng: -118.358080},
+        map: map,
+    });
+}
